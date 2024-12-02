929427e3c88bbea72e02a12e5600e031ada9b609
umesh <umesh98904@gmail.com>
2024-12-02 16:47:06 +0530

    online update

--- src/models/Users.js
+++ src/models/Users.js
@@ -114,7 +114,7 @@ userSchema.pre("save", async function (next) {
 userSchema.pre("save", async function (next) {
   if (this.isModified("userType") && this.userType === "RECEIVER") {
     this.status = "Online";
-    console.log("Updated")
+    console.log("Updated the Status Online RECEIVER ")
   }
   next();
 });
