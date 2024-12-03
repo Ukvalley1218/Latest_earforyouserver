142a1675a637f9ebfef28667eab9ae986ff60ead
umesh <umesh98904@gmail.com>
2024-12-03 18:13:15 +0530

    Reporte_User check user_status in middleware

--- src/middlewares/auth/CheckBlock.js
+++ src/middlewares/auth/CheckBlock.js
@@ -5,6 +5,7 @@ export const checkUserStatus = async (req, res, next) => {
         // Extract the userId from the request object (assumes user is authenticated)
         const userId = req.user._id; // Get user ID from the request (assuming it's set in middleware)
         // If userId is missing, respond with an unauthorized error
+        console.log("checkUserStatus",userId)
         if (!userId) {
             return res.status(401).json({ message: "Unauthorized: User ID is missing." });
         }
