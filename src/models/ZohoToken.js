b048956aea9b1369fe6a97992828e2a0d6235906
umesh <umesh98904@gmail.com>
2025-01-23 16:46:36 +0530

    Zoho

--- src/models/ZohoToken.js
+++ src/models/ZohoToken.js
@@ -1,28 +1,19 @@
 import mongoose from 'mongoose';
 
-// Define the schema for Zoho tokens
-const zohoTokenSchema = new mongoose.Schema(
-  {
-    reason: { 
-      type: String, 
-      required: true, 
-      enum: ['access_token', 'refresh_token'], // You can specify valid values for 'reason' field if needed
-    },
-    token: { 
-      type: String, 
-      required: true, 
-    },
-    createdAt: { 
-      type: Date, 
-      default: Date.now 
-    },
+const zohoTokenSchema = new mongoose.Schema({
+  reason: { 
+    type: String, 
+    required: true, 
+    enum: ['access_token', 'refresh_token']
   },
-  {
-    timestamps: true, // Automatically adds createdAt and updatedAt
+  token: { 
+    type: String, 
+    required: true
   }
-);
+}, {
+  timestamps: true
+});
 
-// Create the ZohoToken model based on the schema
 const ZohoToken = mongoose.models.ZohoToken || mongoose.model('ZohoToken', zohoTokenSchema);
 
 export default ZohoToken;
